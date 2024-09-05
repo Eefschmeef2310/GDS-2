d19f822893504607beddeca33131ab39c8245aeb
Eefschmeef2310 <ethan.lucasawesome@gmail.com>
2024-09-06 00:22:28 +1000

    need to stop understeer

--- kart/kart_controller.gd
+++ kart/kart_controller.gd
@@ -14,7 +14,10 @@ extends VehicleBody3D
 	#@export_subgroup("Subgroup")
 	
 @export_range(0, PI) var max_steer : float = 0.8 #IN RADIANS
-@export var engine_power : int = 300
+@export var max_speed = 500
+@export var engine_power : int = 300:
+	set(value):
+		engine_power = clamp(value, 0, max_speed)
 
 @export_group("Node References")
 @export var camera_pivot : Node3D
@@ -36,10 +39,12 @@ func _physics_process(delta):
 	steering = move_toward(steering, Input.get_axis("Right", "Left") * max_steer, delta * 2.5)
 	engine_force = Input.get_axis("Down", "Up") * engine_power
 	
-	camera_pivot.global_position = camera_pivot.global_position.lerp(global_position, delta * 20.0)
+	camera_pivot.global_position = global_position
+	#camera_pivot.global_position = camera_pivot.global_position.lerp(global_position, delta * 20.0)
 	camera_pivot.transform = camera_pivot.transform.interpolate_with(transform, delta * 5.0)
-	look = look.lerp(global_position + linear_velocity, delta * 5.0)
-	#camera.look(look)
+	look = look.lerp(global_position + linear_velocity, delta * 50.0)
+	#look = global_position + linear_velocity
+	camera.look_at(look)
 	
 #endregion
 
