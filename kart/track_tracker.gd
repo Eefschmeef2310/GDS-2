5fb5af514cef039059f0f61d4a0a4c8c1ce0f9d3
Eefschmeef2310 <ethan.lucasawesome@gmail.com>
2024-09-09 08:22:26 +1000

    updating track system

--- kart/track_tracker.gd
+++ kart/track_tracker.gd
@@ -25,7 +25,8 @@ var closest_point_index : int = 0
 
 #region Godot methods
 func _process(_delta):
-	get_closest_point_index()
+	print(track.curve.get_closest_offset(owner.global_position))
+	#get_closest_point_index()
 #endregion
 
 #region Signal methods
