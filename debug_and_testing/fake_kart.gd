6596f5ddbf5c136e64e4935bf6ce4eae2a672f07
Cookie <51043100+cookiespl@users.noreply.github.com>
2024-09-11 21:24:00 +1000

    fuckass cunt

--- debug_and_testing/fake_kart.gd
+++ debug_and_testing/fake_kart.gd
@@ -1,7 +1,8 @@
 extends RigidBody3D
 
 @export_group("Physics")
-@export var compression_force : float = 1
+@export var suspension_strength : float = 1
+@export var suspension_damping : float = 1
 
 @export_group("Camera")
 @export var camera_pivot : Node3D
@@ -17,19 +18,47 @@ func _ready():
 
 
 func _physics_process(delta: float) -> void:
+	var collision_point : Vector3
+	var collision_normal : Vector3
+	
 	# Floor collisions
 	for ray in wheel_raycasts:
 		if ray.is_colliding():
-			var collision_point = ray.get_collision_point()
-			var collision_normal = ray.get_collision_normal()
+			collision_point = ray.get_collision_point()
+			collision_normal = ray.get_collision_normal()
 			
 			var collision_distance = ray.global_position.distance_to(collision_point)
 			var compression_ratio = 1 - (collision_distance / abs(ray.target_position.y))
 			
-			var force_vector = ray.target_position.normalized() * -1 * compression_force * compression_ratio
-			apply_force(force_vector, ray.position)
+			var velocity_at_point = linear_velocity + angular_velocity.cross(ray.global_position - global_position)
 			
-			# print("Applying force " + str(force_vector))
+			var force_direction = (ray.global_position.direction_to(ray.to_global(ray.target_position))).normalized() * -1
+			#var force_direction = ray.target_position.normalized() * -1
+			# var force_vector = force_direction * suspension_strength * compression_ratio
+			
+			# F = -kx - bv
+			# k = suspension_strength
+			# v = velocity_at_point
+			# b = suspension_dampening
+			# x = compression_ratio
+			var force_vector = (suspension_strength * compression_ratio) - (suspension_damping * velocity_at_point.y)
+			apply_force(force_direction * force_vector, ray.position)
+			print(compression_ratio)
+		
+	if Input.is_action_pressed("Up"):
+		var dir = Vector3.BACK
+		apply_central_force(dir*20)
+	if Input.is_action_pressed("Down"):
+		var dir = Vector3.FORWARD
+		apply_central_force(dir*20)
+	if Input.is_action_pressed("Left"):
+		apply_torque(Vector3.UP * 5)
+	if Input.is_action_pressed("Right"):
+		apply_torque(Vector3.DOWN * 5)
+	
+	if Input.is_action_just_pressed("jump"):
+		apply_central_impulse(Vector3.UP * 20)
+		apply_torque_impulse(Vector3.FORWARD * 0.25 + Vector3.RIGHT * 0.25)
 	
 	# Camera
 	camera_pivot.global_position = global_position
