faa0804e25c057c4a31a116c0b2c968fb9d9fc26
Cookie <51043100+cookiespl@users.noreply.github.com>
2024-09-11 20:04:44 +1000

    suspension raycast works

--- debug_and_testing/fake_kart.gd
+++ debug_and_testing/fake_kart.gd
@@ -1,11 +1,36 @@
 extends RigidBody3D
 
-@export var suspension_length = 0.25
+@export_group("Physics")
+@export var compression_force : float = 1
 
+@export_group("Camera")
 @export var camera_pivot : Node3D
 @export var camera : Camera3D
 
+var wheel_raycasts : Array[RayCast3D]
+
+func _ready():
+	# Find the local bottom corner local coords of the collision box
+	for child in $WheelRaycasts.get_children():
+		wheel_raycasts.append(child)
+	pass
+
 
 func _physics_process(delta: float) -> void:
+	# Floor collisions
+	for ray in wheel_raycasts:
+		if ray.is_colliding():
+			var collision_point = ray.get_collision_point()
+			var collision_normal = ray.get_collision_normal()
+			
+			var collision_distance = ray.global_position.distance_to(collision_point)
+			var compression_ratio = 1 - (collision_distance / abs(ray.target_position.y))
+			
+			var force_vector = ray.target_position.normalized() * -1 * compression_force * compression_ratio
+			apply_force(force_vector, ray.position)
+			
+			# print("Applying force " + str(force_vector))
+	
+	# Camera
 	camera_pivot.global_position = global_position
 	camera_pivot.transform = camera_pivot.transform.interpolate_with(transform, delta * 5.0)
