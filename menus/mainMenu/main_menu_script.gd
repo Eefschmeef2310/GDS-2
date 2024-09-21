290b9e896c8b94b04db85518576c85da79877105
Clevertop <41929769+Clevertop@users.noreply.github.com>
2024-09-21 16:55:20 +1000

    added some autoloads and setup some of main emnu

---
+++ menus/mainMenu/main_menu_script.gd
@@ -0,0 +1,58 @@
+extends Node
+#class_name
+#Authored by Tom. Please consult for any modifications or major feature requests.
+
+#region Variables
+#Signals
+
+#Enums
+
+#Constants
+
+#Exported Variables
+#@export_group("Group")
+#@export_subgroup("Subgroup")
+
+#Onready Variables
+
+#Other Variables (please try to separate and organise!)
+
+#endregion
+
+#region Godot methods
+func _ready():
+	#Runs when all children have entered the tree
+	pass
+
+func _process(delta):
+	#Runs per frame
+	pass
+#endregion
+
+#region Signal methods
+
+func _on_single_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_local_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_onlinebutton_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_settings_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_quit_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+#endregion
+
+#region Other methods (please try to separate and organise!)
+
+#endregion
