c803520f8e586a958ab2349a4b97ada81f3cfd09
Eefschmeef2310 <ethan.lucasawesome@gmail.com>
2024-09-23 09:18:13 +1000

    adding proper stats ui

--- items/upgrades/test_engine_upgrade.gd
+++ items/upgrades/test_engine_upgrade.gd
@@ -5,5 +5,11 @@ extends Upgrade
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	parent.max_speed += speed_increase
-	parent.engine_power -= acceleration_decrease
+	if kart:
+		kart.max_speed += 5
+		kart.acceleration -= 0.5
+
+func _exit_tree() -> void:
+	if kart:
+		kart.max_speed += 5
+		kart.acceleration -= 0.5
