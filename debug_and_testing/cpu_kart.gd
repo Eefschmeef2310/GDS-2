8b20aef50cfe0a642728d6efc7bd8b6f27027a5a
Pihoqub <111715046+Pihoqub@users.noreply.github.com>
2024-09-24 21:14:31 +1000

    CPU kart commit to actual start working

---
+++ debug_and_testing/cpu_kart.gd
@@ -0,0 +1,25 @@
+extends Kart
+class_name CPUKart
+#Authored by AlexV. Please consult for any modifications or major feature requests.
+
+#region Variables
+
+#endregion
+
+#region Godot methods
+func _ready():
+	#Runs when all children have entered the tree
+	pass
+
+func _process(delta):
+	#Runs per frame
+	pass
+#endregion
+
+#region Signal methods
+
+#endregion
+
+#region Other methods (please try to separate and organise!)
+
+#endregion
