de1f38848be76528577173cba6e0588d1932b5de
Eefschmeef2310 <ethan.lucasawesome@gmail.com>
2024-10-30 08:33:43 +1100

    skids

--- debug_and_testing/ball_kart.gd
+++ debug_and_testing/ball_kart.gd
@@ -12,6 +12,7 @@ signal stats_updated()
 
 #Signals for child nodes to hook in to.
 signal drift_started()
+signal drift_continuing()
 signal drift_ended()
 signal boost_started()
 signal boost_ended()
@@ -158,6 +159,7 @@ func _physics_process(delta: float) -> void:
 		# Perform jump animation here
 		
 		if drifting:
+			drift_continuing.emit()
 			var control : float = (remap_axis(steer_axis, .4, 2)) if (drift_direction == 1) else (remap_axis(steer_axis, 2, .4))
 			var power_control : float = remap_axis(steer_axis, .5, 1) if (drift_direction == 1) else remap_axis(steer_axis, 1, .5)
 			steer(drift_direction, control)
